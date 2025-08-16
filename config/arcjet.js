de745df4f6477023d56bbab7194da4e2de8e3282
Ajijola Adeola <aajijola3@gmail.com>
2025-08-16 17:06:40 +0100

    pm2 install

--- config/arcjet.js
+++ config/arcjet.js
@@ -7,19 +7,19 @@ const aj = arcjet({
   characteristics: ["ip.src"],
   rules: [
     shield({ mode: "LIVE" }),
-    detectBot({
-      mode: "LIVE",
-      allow: [
-        "CATEGORY:SEARCH_ENGINE",
-        "PostmanRuntime/*",
-        "CATEGORY:API", // This should cover Postman and other API testing tools
-        "*Postman*", // More flexible Postman matching
-      ],
-    }),
     // detectBot({
     //   mode: "LIVE",
-    //   allow: ["CATEGORY:SEARCH_ENGINE"],
+    //   allow: [
+    //     "CATEGORY:SEARCH_ENGINE",
+    //     "PostmanRuntime/*",
+    //     "CATEGORY:API", // This should cover Postman and other API testing tools
+    //     "*Postman*", // More flexible Postman matching
+    //   ],
     // }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE"],
+    }),
     tokenBucket({
       mode: "LIVE",
       refillRate: 5,
